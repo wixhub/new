09746b41129d5d0624cd5df374c4187bbe481775
wixhub <rublin@gmx.de>
2026-08-02 12:00:00 +0200

    feat: implement Charts component with styling, testing, and data handling

---
+++ src/app/features/charts/charts.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Charts } from './charts';
+
+describe('Charts', () => {
+  let component: Charts;
+  let fixture: ComponentFixture<Charts>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Charts],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Charts);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
