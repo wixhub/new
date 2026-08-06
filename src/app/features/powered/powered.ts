bfe95fa94bbfb54e4c033764d2d197f7a71cdc55
wixhub <rublin@gmx.de>
2026-08-06 16:58:30 +0200

    feat: add Powered component with HTML, SCSS, and unit tests

---
+++ src/app/features/powered/powered.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-powered',
+  imports: [],
+  templateUrl: './powered.html',
+  styleUrl: './powered.scss',
+})
+export class Powered {}
